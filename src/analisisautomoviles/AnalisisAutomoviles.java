5927a3eb58cf06a7ea4dc4410ed99df79d6a42ca
AKM451 <102634086+AKM451@users.noreply.github.com>
2022-04-07 14:44:05 -0500

    Correciones Maestro

--- src/analisisautomoviles/AnalisisAutomoviles.java
+++ src/analisisautomoviles/AnalisisAutomoviles.java
@@ -1,50 +1,48 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package analisisautomoviles;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Kevin
+ * @author Angel
  */
 public class AnalisisAutomoviles {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Coche car01 = new Coche();
-        car01.prender = true;
-        car01.kilometraje = 10000;
-        car01.luz_delantera = true;
-        car01.luz_freno = false;
-        car01.direccionales = 9;
-        car01.movimiento = true;
-        System.out.println("El carro esta " + (car01.prender ? "encendido" : "apagado"));
-        System.out.println("El kilometraje es de " + car01.kilometraje);
-        System.out.println("La luz delantera esta " + (car01.luz_delantera ? "encendida" : "apagada"));
-        System.out.println("La luz de freno esta " + (car01.luz_freno ? "encendida" : "apagada"));
-        System.out.println("Las direccionales estan " + (car01.direccionales == 1 ? "del lado izquierdo"
-                : car01.direccionales == 2 ? "del lado derecho" : "apagadas"));
-        System.out.println("El carro esta " + (car01.movimiento ? "desplazamiento" : "inmovil"));
+        Coche c1 = new Coche();
+        String acelera;
+        /*
+        double velocidad = Double.parseDouble(JOptionPane.showInputDialog("Velocidad:"));
+        double tiempo = Double.parseDouble(JOptionPane.showInputDialog("Tiempo:"));
+        //c1.prende = true;
+        acelera = c1.acelerar(velocidad, tiempo);
 
-        System.out.println("");
+        if(!acelera.equals(""))
+            JOptionPane.showMessageDialog(null, acelera, "Error", 
+                    JOptionPane.ERROR_MESSAGE);
+*/
+/*
+        c1.setLuz(true, true);
+        c1.setLuz(false, true);
+*/
+        //c1.setLuz_delantera(true);
+        //c1.setLuz_trasera(false);
+        c1.enciendeLuzDelantera();
+        c1.enciendeLuzTrasera();
 
-        Coche car02 = new Coche();
-        car02.prender = false;
-        car02.kilometraje = 15000;
-        car02.luz_delantera = false;
-        car02.luz_freno = true;
-        car02.direccionales = 2;
-        car02.movimiento = false;
-        System.out.println("El carro esta " + (car02.prender ? "encendido" : "apagado"));
-        System.out.println("El kilometraje es de " + car02.kilometraje);
-        System.out.println("La luz delantera esta " + (car02.luz_delantera ? "encendida" : "apagada"));
-        System.out.println("La luz de freno esta " + (car02.luz_freno ? "encendida" : "apagada"));
-        System.out.println("Las direccionales estan " + (car02.direccionales == 1 ? "del lado izquierdo"
-                : car02.direccionales == 2 ? "del lado derecho" : "apagadas"));
-        System.out.println("El carro esta " + (car02.movimiento ? "desplazamiento" : "inmovil"));
+        JOptionPane.showMessageDialog(null, c1, "Info", JOptionPane.INFORMATION_MESSAGE);
+        c1.cambiaEstadoLuzDelantera();
+        JOptionPane.showMessageDialog(null, c1, "Info", JOptionPane.INFORMATION_MESSAGE);
+        c1.cambiaEstadoLuzDelantera();
+        JOptionPane.showMessageDialog(null, c1, "Info", JOptionPane.INFORMATION_MESSAGE);
     }
+    
 }
--- src/analisisautomoviles/Coche.java
+++ src/analisisautomoviles/Coche.java
@@ -1,20 +1,211 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package analisisautomoviles;
 
 /**
  *
- * @author Kevin
+ * @author Angel
  */
 public class Coche {
+    //Atributos
+    private boolean prende, luz_delantera, luz_trasera;
+    private boolean movimiento;
+    private double kilometraje;
+    private Direccionales direccionales;
+    private double velocidad;
 
-    boolean prender;
-    double kilometraje;
-    boolean luz_delantera;
-    boolean luz_freno;
-    int direccionales;
-    boolean movimiento;
+    //Constructor
+    /**
+     */
+    public Coche(){
+        this.velocidad = 0.0;
+        this.direccionales = Direccionales.APAGADAS;
+        this.kilometraje = 0;
+        this.luz_delantera = false;
+        this.luz_trasera = false;
+        this.prende = false;
+        this.movimiento = false;
+    }
+    
+    //Getters y Setters
+    public double getKilometraje() {
+        return kilometraje;
+    }
 
+    public double getVelocidad() {
+        return velocidad;
+    }
+/*
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+*/
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+    
+    public boolean isMovimiento() {
+        return movimiento;
+    }
+    
+    public String estadoMovimiento(){
+        return this.movimiento ? "en movimiento" : "detenido";
+    }
+
+    public void setMovimiento(boolean movimiento) {
+        this.movimiento = movimiento;
+        if(!this.prende) //this.prende != true
+            this.movimiento = false;
+    }
+
+    public String getDireccionales() {
+        return direccionales.getEstado();
+    }
+    
+    public void setDireccionales(int direccionales){
+        if(direccionales < Direccionales.values().length){
+            for(Direccionales d : Direccionales.values())
+                if(direccionales == d.ordinal())
+                    this.direccionales = d;
+        } else
+            this.direccionales = Direccionales.APAGADAS;
+    }
+
+    public void setDireccionales(Direccionales direccionales){
+        this.direccionales = direccionales;
+    }
+
+    public boolean isCochePrendido() {
+        return prende;
+    }
+
+    public String estadoCoche(){
+        return (this.prende ? "encendido" : "apagado");
+    }
+    
+    public boolean isLuzDelanteraEncendida() {
+        return luz_delantera;
+    }
+
+    public String estadoLuzDelantera(){
+        return (this.luz_delantera ? "encendida" : "apagada");
+    }
+    
+    public boolean isLuzTraseraEncendida() {
+        return luz_trasera;
+    }
+
+    public String estadoLuzTrasera(){
+        return (this.luz_trasera ? "encendida" : "apagada");
+    }
+    
+/*    
+    public void setPrendeCoche(boolean prende) {
+        this.prende = prende;
+    }
+    */
+    public void enciendeCoche() {
+        this.prende = true;
+    }
+
+    public void apagaCoche() {
+        this.prende = false;
+    }
+
+    public void cambiaEstadoCoche() {
+        this.prende = !this.prende;
+    }
+
+/*
+    /**
+     * Solo Dios y yo sabíamos para qué servía esta cosa, ahora, solo Dios lo sabe
+     * @param delantera true para delantera, false para trasera
+     * @param luz true para encendida, false para apagada
+     *//*
+    public void setLuz(boolean delantera, boolean luz) {
+        if(delantera)
+            this.luz_delantera = luz;
+        else
+            this.luz_trasera = luz;
+    }
+
+    public void setLuz_delantera(boolean luz_delantera) {
+        this.luz_delantera = luz_delantera;
+    }
+*/
+    //probar implementación con switch ... case
+    public void enciendeLuzDelantera() {
+        this.luz_delantera = true;
+    }
+
+    public void apagaLuzDelantera() {
+        this.luz_delantera = false;
+    }
+
+    public void cambiaEstadoLuzDelantera() {
+        this.luz_delantera = !this.luz_delantera;
+    }
+
+/*
+    public void setLuz_trasera(boolean luz_trasera) {
+        this.luz_trasera = luz_trasera;
+    }
+*/  
+    public void enciendeLuzTrasera() {
+        this.luz_trasera = true;
+    }
+
+    public void apagaLuzTrasera() {
+        this.luz_trasera = false;
+    }
+
+    public void cambiaEstadoLuzTrasera() {
+        this.luz_trasera = !this.luz_trasera;
+    }
+
+    //Otros métodos
+    public String acelerar(){
+        if(this.prende){
+            this.movimiento = true;
+            this.velocidad = 5;
+            this.kilometraje += this.velocidad; //Aceleración instantánea de 1 segundo
+            return "";
+        } else
+            return "No puede acelerar, el vehículo está apagado o algún argumento es cero";
+    }
+
+    public String acelerar(double segundos){
+        if(this.prende && segundos > 0){
+            this.movimiento = true;
+            this.velocidad = 5;
+            this.kilometraje += this.velocidad * segundos;// v=d/t d = vt acumulador  
+            return "";
+        } else
+            return "No puede acelerar, el vehículo está apagado o algún argumento es cero";
+    }
+
+    public String acelerar(double metros_seg, double segundos){
+        if(this.prende && metros_seg > 0 && segundos > 0){
+            this.movimiento = true;
+            this.velocidad = metros_seg;
+            this.kilometraje += this.velocidad * segundos;// v=d/t d = vt acumulador  
+            return "";
+        } else
+            return "No puede acelerar, el vehículo está apagado o algún argumento es cero";
+    }
+
+    //toString
+    @Override
+    public String toString(){
+        return "El coche está " + this.estadoCoche() +
+            "\nEl coche está " + this.estadoMovimiento() +
+            "\nEl kilometraje es " + this.kilometraje + " m" +
+            "\nLa velocidad es " + this.velocidad + " m/s" +
+            "\nLa luz delantera está " + this.estadoLuzDelantera() +
+            "\nLa luz trasera está " + this.estadoLuzTrasera() +
+            "\nLas direccionales están " + this.getDireccionales();
+    }
 }
